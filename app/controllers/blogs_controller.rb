9be0fa63ffc9db1b57242be3d72791459c322bd7
Austin Adams <caustinwadams@gmail.com>
2019-10-08 18:43:48 -0400

    Added blog model and controller

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,11 @@
+class BlogsController < ApplicationController
+  def new
+  end
+
+  def index
+    @blogs = Blog.all
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'blogs/new'
+  get 'blogs/index'
+  get 'blogs/show'
   get 'static_pages/home'
   get 'static_pages/resume'
   root 'static_pages#home'
   get '/resume', to: 'static_pages#resume'
+  get '/blog',   to: 'blogs#index'
 end
