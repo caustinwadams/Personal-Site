d214da99713a0dd65745ad946c9521a004d93ff7
Austin Adams <caustinwadams@gmail.com>
2019-10-08 16:33:33 -0400

    Added resume page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  # Changes navbar link to 'active' if the link_path
+  # is the current page
+  def active_class(link_path)
+    current_page?(link_path) ? "nav-item active" : "nav-item"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'static_pages/home'
+  get 'static_pages/resume'
   root 'static_pages#home'
-
+  get '/resume', to: 'static_pages#resume'
 end
