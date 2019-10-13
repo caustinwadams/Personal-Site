5f2c60e5fce3ad8de13a29022c077dea5d4e1450
Austin Adams <caustinwadams@gmail.com>
2019-10-13 00:42:03 -0400

    First bit of progress on method for pretty HTML in blog posts

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,14 @@
 module BlogsHelper
+
+  # Makes HTML in a blog post more pretty
+  # TODO: Allow for code, underlines, etc... 
+  # (and return the string instead of just printing)
+  def make_html(post)
+    # Replaces text for bold input
+    if post =~ /(\*\*(.+)\*\*)/m
+      post.gsub!($1, "<strong>#{$2}</strong>")
+    end
+    puts post
+  end
+
 end
