6abee83ab6a745dc90fa00df0019e7acc62b1ceb
Austin Adams <caustinwadams@gmail.com>
2019-10-14 17:19:21 -0400

    Added user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_08_221320) do
+ActiveRecord::Schema.define(version: 2019_10_14_205933) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.text "post"
     t.datetime "published_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
